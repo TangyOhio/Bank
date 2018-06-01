03ff02bcccd68319e865c95acf070374fcf1a357
Jace <jacemcgough1@gmail.com>
2018-06-01 15:32:31 -0600

    Took out profiles, lul

--- db/migrate/20180601210943_create_profiles.rb
+++
@@ -1,9 +0,0 @@
-class CreateProfiles < ActiveRecord::Migration[5.2]
-  def change
-    create_table :profiles do |t|
-      t.belongs_to :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
